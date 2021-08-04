b867b87847ea4d4fe9e1d68de1108524070640a6
barbaraaliverti@gmail.com <barbaraaliverti@gmail.com>
2021-08-04 20:18:33 -0300

    M1A2

---
+++ src/components/commons/Menu/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Logo } from '../../../theme/Logo';
+import { MenuWrapper } from './styles/MenuWrapper';
+
+const links = [
+  {
+    text: 'Home',
+    url: '/'
+  },
+  {
+    text: 'Perguntas frequentes',
+    url: '/faq'
+  },
+  {
+    text: 'Sobre',
+    url: '/sobre'
+  }
+]
+
+
+export default function Menu() {
+  return(
+    <MenuWrapper>
+      <MenuWrapper.LeftSide><Logo /></MenuWrapper.LeftSide>
+      <MenuWrapper.CentralSide>{links.map((link)=> {
+        return(
+          <li>
+            <a href={link.url}>
+              {link.text}
+            </a>          
+          </li>
+        )
+      })}</MenuWrapper.CentralSide>
+      <MenuWrapper.RightSide>
+        <button>Entrar</button>
+        <button>Cadastrar</button>
+      </MenuWrapper.RightSide>
+    </MenuWrapper>
+  )  
+};
