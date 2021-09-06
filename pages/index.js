4c8c99715707f1f7851eb39453a1badf81d23f86
barbaraaliverti@gmail.com <barbaraaliverti@gmail.com>
2021-09-06 16:43:05 -0300

    details on websitepage wrapper

--- pages/index.js
+++ pages/index.js
@@ -2,61 +2,26 @@
 import React, { useContext } from 'react';
 import Text from '../src/components/foundation/Text';
 import Grid from '../src/components/foundation/Layout/Grid';
-
 import WebsitePageWrapper, { WebsitePageContext } from '../src/components/Wrappers/WebsitePageWrapper/WebsitePageWrapper';
 import Button from '../src/components/commons/Button';
-
-import SEO from '../src/components/commons/SEO';
-
+import Box from '../src/components/foundation/Layout/Box';
 
 const HomeScreen = () => {
   const webpagePageContext = useContext(WebsitePageContext);
 
   return (
-    <Grid.Container
-      marginTop={{
-        xs: '32px',
-        md: '75px',
-      }}
+    <Box
+      flex={1}
+      display="flex"
+      flexDirection="column"
     >
-
-      <Grid.Row>
-        <Grid.Col
-          offset={{ xs: 0, md: 1 }}
-          value={{ xs: 12, md: 5 }}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          flexDirection="column"
-        >
-          <Text
-            variant="title"
-            tag="h1"
-            color="tertiary.main"
-            textAlign={{
-              xs: 'center',
-              md: 'left',
-            }}
-          >
-            Compartilhe momentos e conecte-se com amigos
-          </Text>
-          <Text
-            variant="paragraph1"
-            tag="p"
-            color="tertiary.light"
-            textAlign={{
-              xs: 'center',
-              md: 'left',
-            }}
-
-      <SEO headTitle="Home" />
-      
       <Grid.Container
         marginTop={{
           xs: '32px',
           md: '75px',
         }}
       >
+
         <Grid.Row>
           <Grid.Col
             offset={{ xs: 0, md: 1 }}
@@ -65,41 +30,72 @@ const HomeScreen = () => {
             alignItems="center"
             justifyContent="center"
             flexDirection="column"
-
           >
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
-          </Text>
+            <Text
+              variant="title"
+              tag="h1"
+              color="tertiary.main"
+              textAlign={{
+                xs: 'center',
+                md: 'left',
+              }}
+            >
+              Compartilhe momentos e conecte-se com amigos
+            </Text>
+            <Text
+              variant="paragraph1"
+              tag="p"
+              color="tertiary.light"
+              textAlign={{
+                xs: 'center',
+                md: 'left',
+              }}
+            >
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
+            </Text>
 
-          <Button
-            variant="primary.main"
-            margin={{
-              xs: 'auto',
-              md: 'initial',
-            }}
-            display="block"
-            onClick={() => { webpagePageContext.toggleModalCadastro(); }}
+            <Button
+              variant="primary.main"
+              margin={{
+                xs: 'auto',
+                md: 'initial',
+              }}
+              display="block"
+              onClick={() => {
+                webpagePageContext.toggleModalCadastro();
+              }}
+            >
+              Cadastrar
+            </Button>
+          </Grid.Col>
+          <Grid.Col
+            value={{ xs: 12, md: 6 }}
           >
-            Cadastrar
-          </Button>
-        </Grid.Col>
-        <Grid.Col
-          value={{ xs: 12, md: 6 }}
-        >
-          <img
-            alt="Imagens de celular com páginas internas do aplicativo Instalura mostrando o perfil do ator Nicolas Cage"
-            style={{ display: 'block', margin: 'auto' }}
-            src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
-          />
-        </Grid.Col>
-      </Grid.Row>
-    </Grid.Container>
+            <img
+              alt="Imagens de celular com páginas internas do aplicativo Instalura mostrando o perfil do ator Nicolas Cage"
+              style={{ display: 'block', margin: 'auto' }}
+              src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
+            />
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
+    </Box>
   );
 };
 
 export default function Home() {
   return (
-    <WebsitePageWrapper>
+    <WebsitePageWrapper
+      seoProps={{
+        headTitle: 'Home',
+      }}
+      pageBoxProps={{
+        backgroundImage: 'url(/images/bubbles.svg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'bottom right',
+      }}
+    >
       <HomeScreen />
     </WebsitePageWrapper>
   );
--- src/components/Wrappers/WebsitePageWrapper/WebsitePageWrapper.js
+++ src/components/Wrappers/WebsitePageWrapper/WebsitePageWrapper.js
@@ -6,12 +6,18 @@ import Menu from '../../commons/Menu';
 import Modal from '../../commons/Modal';
 import Box from '../../foundation/Layout/Box';
 import FormSignup from '../../patterns/FormSignup';
+import SEO from '../../commons/SEO';
 
 export const WebsitePageContext = createContext({
   toggleModalCadastro: () => {},
 });
 
-const WebsitePageWrapper = ({ children }) => {
+const WebsitePageWrapper = ({
+  children,
+  seoProps,
+  pageBoxProps,
+  menuProps,
+}) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
@@ -22,11 +28,14 @@ const WebsitePageWrapper = ({ children }) => {
         },
       }}
     >
+      <SEO {...seoProps} />
+
       <Box
         flex={1}
         display="flex"
         flexWrap="wrap"
         flexDirection="column"
+        {...pageBoxProps}
       >
         <Modal
           isOpen={isModalOpen}
@@ -43,9 +52,12 @@ const WebsitePageWrapper = ({ children }) => {
           )}
 
         </Modal>
+        {menuProps.display && (
         <Menu
           onCadastrarClick={() => setIsModalOpen(true)}
         />
+        )}
+
         {children}
         <Footer />
       </Box>
@@ -53,7 +65,26 @@ const WebsitePageWrapper = ({ children }) => {
   );
 };
 
+WebsitePageWrapper.defaultProps = {
+  seoProps: {},
+  pageBoxProps: {},
+  menuProps: {
+    display: true,
+  },
+};
+
 WebsitePageWrapper.propTypes = {
+  seoProps: PropTypes.shape({
+    headTitle: PropTypes.string,
+  }),
+  menuProps: PropTypes.shape({
+    display: PropTypes.bool,
+  }),
+  pageBoxProps: PropTypes.shape({
+    backgroundImage: PropTypes.string,
+    backgroundRepeat: PropTypes.string,
+    backgroundPosition: PropTypes.string,
+  }),
   children: PropTypes.node.isRequired,
 };
 
