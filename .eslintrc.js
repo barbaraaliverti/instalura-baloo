1a153acd8d95ccc32bd0b6f2b3d0c5d9b0300c98
barbaraaliverti@gmail.com <barbaraaliverti@gmail.com>
2021-09-21 21:19:36 -0300

    added cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    'cypress/global': true,
   },
   extends: [
     'plugin:cypress/recommended',
--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -1,23 +1,22 @@
-import { test } from "shelljs"
+import { test } from 'shelljs';
 
 /// <reference types="cypress" />
 
 describe('/pages/app/login', () => {
-    //cada it == um teste
-    it('preencha os campos e vá pra página de perfil /app/profile', () => {
-        cy.visit('/app/login');
+  // cada it == um teste
+  it('preencha os campos e vá pra página de perfil /app/profile', () => {
+    cy.visit('/app/login');
 
-        //preencher input usuário
-        cy.get('#formCadastro input[name="usuario"]').type('baloozinha')
+    // preencher input usuário
+    cy.get('#formCadastro input[name="usuario"]').type('baloozinha');
 
-        //preencher input senha
-        cy.get('#formCadastro input[name="senha"]').type('senhasegura')
+    // preencher input senha
+    cy.get('#formCadastro input[name="senha"]').type('senhasegura');
 
-        //clicar em submit
-        cy.get('#formCadastro button[type="submit"]').click()
+    // clicar em submit
+    cy.get('#formCadastro button[type="submit"]').click();
 
-        //o que se espera? ir para /app/profile
-        cy.url().should('include', '/app/profile');
-
-    })
-})
+    // o que se espera? ir para /app/profile
+    cy.url().should('include', '/app/profile');
+  });
+});
