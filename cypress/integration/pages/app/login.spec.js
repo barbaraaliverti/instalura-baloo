c801deac92fe00c7fc9e376bff9862a346cfafb2
barbaraaliverti@gmail.com <barbaraaliverti@gmail.com>
2021-09-27 16:29:47 -0300

    added nookie and saved cookie

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -3,10 +3,14 @@
 describe('/pages/app/login', () => {
   // cada it == um teste
   it('preencha os campos e vá pra página de perfil /app/profile', () => {
+    // intercepta a chamada de API
+    cy.intercept('https://instalura-api-git-master-omariosouto.vercel.app/api/login')
+      .as('userLogin');
+
     cy.visit('/app/login');
 
     // preencher input usuário
-    cy.get('#formCadastro input[name="usuario"]').type('baloozinha');
+    cy.get('#formCadastro input[name="usuario"]').type('omariosouto');
 
     // preencher input senha
     cy.get('#formCadastro input[name="senha"]').type('senhasegura');
@@ -16,5 +20,16 @@ describe('/pages/app/login', () => {
 
     // o que se espera? ir para /app/profile
     cy.url().should('include', '/app/profile');
+
+    // verifica se tem o token
+    cy.wait('@userLogin')
+      .then((intercept) => {
+        const { token } = intercept.response.body.data;
+        cy.getCookie('APP_TOKEN')
+        // token existe?
+          .should('exist')
+          // token armazenado no cookie é igual ao token do server?
+          .should('have.property', 'value', token);
+      });
   });
 });
